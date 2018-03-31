7bca6eb9c495147ac44b05fd08b59b39d74fa5c3
Philippe Bruhat (BooK) <book@cpan.org>
2018-03-31 10:18:40 +0200

    Implement File::ContentStore as a directory of files named after their content's digest
    link_file() hard links a file to its digest name in the content store.

---
+++ lib/File/ContentStore.pm
@@ -0,0 +1,54 @@
+package File::ContentStore;
+
+use Carp qw( croak );
+use Path::Tiny;
+use Digest;
+
+use Moo;
+use namespace::clean;
+
+has path => (
+    is       => 'ro',
+    required => 1,
+    coerce   => sub { Path::Tiny->new( shift ) },
+);
+
+has digest => (
+    is      => 'ro',
+    default => 'SHA-1',
+);
+
+has parts => (
+    is => 'lazy',
+    builder =>
+      sub { int( length( Digest->new( shift->digest )->hexdigest ) / 32 ) },
+    init_arg => undef,
+);
+
+my $BUFF_SIZE = 1024 * 32;
+my $DIGEST_OPTS = { chunk_size => $BUFF_SIZE };
+
+sub link_file {
+    my ($self, $file) = @_;
+    $file = Path::Tiny->new($file);
+
+    # compute content file name
+    my $digest = $file->digest( $DIGEST_OPTS, $self->digest );
+    my $content =
+      $self->path->child(
+        map( { substr $digest, 2 * $_, 2 } 0 .. $self->parts - 1 ),
+        substr( $digest, 2 * $self->parts ) );
+    $content->parent->mkpath;
+
+    # link both files
+    my ( $old, $new ) = -e $content ? ( $content, $file ) : ( $file, $content );
+
+    return if $old eq $new;    # do not link a file to itself
+    unlink $new if -e $new;
+    link $old, $new or croak "Failed linking $new to to $old: $!";
+    chmod 0444, $old;
+
+    return $content;
+}
+
+1;
