faa8a68ea0774aa8b67c44ce00c185943d90bd12
Philippe Bruhat (BooK) <book@cpan.org>
2019-11-25 23:43:28 +0100

    Update copyright

--- lib/File/ContentStore.pm
+++ lib/File/ContentStore.pm
@@ -432,7 +432,7 @@ Philippe Bruhat (BooK) <book@cpan.org>.
 
 =head1 COPYRIGHT
 
-Copyright 2018 Philippe Bruhat (BooK), all rights reserved.
+Copyright 2018-2019 Philippe Bruhat (BooK), all rights reserved.
 
 =head1 LICENSE
 
